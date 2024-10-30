d7892fea84f86bd1dad5794d410f7641619dabf9
vergilx6x <med.work96@hotmail.com>
2024-10-30 19:41:30 +0100

    Task 2

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" BasicCache module using the FIFO policy.
+"""
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class LIFOCache(BaseCaching):
+    """Stores and retirevies an item from cache
+    """
+    def __init__(self):
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """Adds an item in the cache.
+        """
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                last_key, _ = self.cache_data.popitem(True)
+                print("DISCARD:", last_key)
+        self.cache_data[key] = item
+        self.cache_data.move_to_end(key, last=True)
+
+    def get(self, key):
+        """Retrieves an item by key.
+        """
+        return self.cache_data.get(key, None)
