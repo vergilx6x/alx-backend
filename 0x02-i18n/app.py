0cf3babbb332784d47340e4acb884021fd51de2d
vergilx6x <med.work96@hotmail.com>
2024-11-05 19:47:27 +0100

    Fix

--- 0x02-i18n/app.py
+++ 0x02-i18n/app.py
@@ -7,7 +7,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    """The home/index page.
+    """
+    return render_template('0-index.html')
 
 
 if __name__ == '__main__':
